84139e281bdff94632d6ca06023f2675b4d1c5a7
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-06-18 05:50:21 +0200

    implementato form di registrazione utenti

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { JsonLogin } from '../../dto/JsonLogin';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+
+  username: string = '';
+  password: string = '';
+
+  constructor (private http:HttpClient, private router:Router) {}
+
+  register() {
+    if (this.username != '' && this.password != '') {
+      const jsonRegister = new JsonLogin(this.username.toString(), this.password.toString());
+      this.http.post('http://localhost:8080/api/register',jsonRegister).subscribe(res => {
+        this.registerSuccess();
+      });
+    }
+  }
+
+  registerSuccess() {
+    alert("ti sei registrato correttamente ora puoi accedere!");
+  }
 
 }
